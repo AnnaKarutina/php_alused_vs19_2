329d4814bc6c628217ec9731ae90b38835340282
Anna Karutina <anna.karutina@khk.ee>
2019-12-03 13:33:33 +0200

    Tekstitöötlus - ülesanne 9 - näidislahendused

---
+++ text/form.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * file name: form.php;
+ * autor: anna.karutina;
+ * date: 03.12.2019;
+ */
+?>
+<form action="challenge.php" method="get">
+  Sisesta oma nimi: <input type="text" name="nimi">
+</form>
